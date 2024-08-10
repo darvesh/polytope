4189ff264def3755cfb0b66c224042040adbb8f5
Darvesh <hello@darve.sh>
2024-08-11 04:07:38 +0530

    remove unused functions

--- src/util.ts
+++ src/util.ts
@@ -1,27 +1,10 @@
 import { possibly, sequenceOf, type Parser } from "arcsecond";
 
-export const makeBasicType =
-	(typeName: string) =>
-	<T>(value: T) => ({
-		type: typeName,
-		value,
-		toString: () => `${typeName}(${value})`,
-	});
-
 export const orEmptyString = (parser: Parser<string, string, string>) =>
 	possibly(parser).map((x: any) => (x ? x : ""));
 
 export const joinedSequence = (parsers: Parser<string, string, string>[]) =>
 	sequenceOf(parsers).map((x) => x.join(""));
-
-export const makeMultiType =
-	(typeName: string) =>
-	<T>(values: unknown) => ({
-		type: typeName,
-		value: values,
-		toString: () => `${typeName}(${values})`,
-	});
-
 export function isStringType(value: any): value is string {
 	return typeof value === "string";
 }
